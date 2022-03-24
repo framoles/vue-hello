6833188ee4be680d2a66e316ca1d73563277fc3d
Francesco Moles <francesco.moles@outlook.it>
2022-03-24 16:27:26 +0100

    title+image

---
+++ script.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+
+    el: "#vue-app",
+    data: {
+        title: "Hello world!",
+        image: "https://i.picsum.photos/id/962/400/400.jpg?hmac=gCXSUrSHf6XumFml2-iicMnVPIsWYIIDSqKwMUvaKUY"
+    }
+
+})
